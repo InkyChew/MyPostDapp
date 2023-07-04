ac09458fd38c19712da57e19ac5d3a5f2da94903
InkyChew <inky11308@gmail.com>
2023-07-04 10:40:40 +0800

    Auto-generated commit [ci skip]

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  CHAINID: "0xaa36a7",
-  ALCHEMY_URL: "wss://eth-sepolia.g.alchemy.com/v2/l5vB7xpo6wBFnzq3N43QdZVHfH3DlBiT",
-  CONTRACT_ADDRESS: "0x653850b7fcfE2F426588D013d99759a3f5d5aF0b"
-};
